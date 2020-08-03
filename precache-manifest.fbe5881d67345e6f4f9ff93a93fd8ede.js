3fc9639fa6f7acde9211ab02da8b5d2a3b37207f
unknown <harsharay.ray@gmail.com>
2020-08-03 21:03:32 +0530

    Updates

---
+++ precache-manifest.fbe5881d67345e6f4f9ff93a93fd8ede.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f90419f501a6bd0400efcdc749eb4c8",
+    "url": "/cric-info/index.html"
+  },
+  {
+    "revision": "9f02dcb156ef3f5345d2",
+    "url": "/cric-info/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d822dad1974ae195a74f",
+    "url": "/cric-info/static/js/2.46c76ec1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cric-info/static/js/2.46c76ec1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f02dcb156ef3f5345d2",
+    "url": "/cric-info/static/js/main.44d4c0f3.chunk.js"
+  },
+  {
+    "revision": "1b7e952eb6a24e38c045",
+    "url": "/cric-info/static/js/runtime-main.4bbddc9a.js"
+  }
+]);
